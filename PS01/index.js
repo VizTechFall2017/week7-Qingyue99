52814ff146d42814b568404e8efadf2ffb4a50b6
Qingyue99 <31904741+Qingyue99@users.noreply.github.com>
2017-10-30 11:57:36 -0400

    noon

--- PS01/index.js
+++ PS01/index.js
@@ -1,7 +1,7 @@
 var svg = d3.select("svg"),
     margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 900 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom,
+    width = 900 - margin.left - margin.right-50,
+    height = 650 - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var x = d3.scaleBand()
@@ -31,6 +31,14 @@ d3.csv("2chart.csv",
     yaxis.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
     z.domain(keys);
 
+    var focus = svg.append("g")
+            .attr("class", "focus")
+            .style("display", "none");
+
+    focus.append("text")
+            .attr("x", 9)
+            .attr("dy", ".35em");
+
     g.append("g")
         .selectAll("g")
         .data(d3.stack().keys(keys)(data))
@@ -51,6 +59,8 @@ d3.csv("2chart.csv",
 
             currentClass = d3.select(this).attr('class');
             svg.selectAll('.' + currentClass).attr('stroke','red').attr('stroke-width','2');
+         //   focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
+         //   focus.select("text").text("yyyy");
         })
         .on('mouseout', function(d){
             d3.select(this).attr('stroke','').attr('stroke-width','0');;
@@ -74,6 +84,18 @@ d3.csv("2chart.csv",
         .attr("font-weight", "bold")
         .attr("text-anchor", "start")
         .text("Number of People");
+
+        g.append("g")
+            .attr("class", "axis")
+            .call(d3.axisLeft(yaxis).ticks(null, "s"))
+            .append("text")
+            .attr("x",800)
+            .attr("y", 600)
+            .attr("dy", "0.32em")
+            .attr("fill", "#000")
+            .attr("font-weight", "bold")
+            .attr("text-anchor", "start")
+            .text("Spend");
 
 
     var legend = g.append("g")
